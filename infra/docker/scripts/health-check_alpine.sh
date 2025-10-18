58ce9f79e9d454042a728c55cd52758cecf5e7d4
carielflore <carielflore@gmail.com>
2025-10-18 23:49:23 +0300

    fix health-check_alpine.sh to use service names

--- infra/docker/scripts/health-check_alpine.sh
+++ infra/docker/scripts/health-check_alpine.sh
@@ -7,7 +7,7 @@ cd "$DOCKER_DIR"
 
 echo "Docker services health check:"
 
-services="django:8000/ nginx:8080/ prometheus:9090/-/healthy grafana:3000/api/health node_exporter:9100/metrics nginx_exporter:9113/metrics"
+services="django:8000/ nginx:80	/ prometheus:9090/-/healthy grafana:3000/api/health node_exporter:9100/metrics nginx_exporter:9113/metrics"
 
 all_healthy=true
 
@@ -15,7 +15,7 @@ for service in $services; do
     name="${service%:*}"
     endpoint="${service#*:}"
 
-    if curl -s -f --connect-timeout 5 "http://localhost:${endpoint}" > /dev/null; then
+    if curl -s -f --connect-timeout 5 "http://${name}:${endpoint}" > /dev/null; then
         echo "$name is healthy"
     else
         echo "$name health check is failed"
